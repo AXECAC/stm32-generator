674d2bee2c45e71064186d0f3251a9952e97bcaf
aragami3070 <smirnov17612@gmail.com>
2026-07-01 02:18:34 +0400

    feat(gpio): add ChosenPinWithMode and ChosenPin

--- src/core/gpio/mod.rs
+++ src/core/gpio/mod.rs
@@ -2,7 +2,14 @@ use crate::core::gpio::f4::{StmF4PinMode, f401::StmF401Pin};
 
 pub mod f4;
 
+/// Пин
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum ChosenPin {
+    StmF401(StmF401Pin),
+}
+
+/// Пин + режим
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ChosenPinWithMode {
     StmF401(StmF401Pin, StmF4PinMode),
 }
