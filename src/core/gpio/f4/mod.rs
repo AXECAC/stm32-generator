d48cce581eab719cd29c79cb0b3fdf7be446414a
aragami3070 <smirnov17612@gmail.com>
2026-07-01 02:00:12 +0400

    feat(gpio-f4): add output speed

--- src/core/gpio/f4/mod.rs
+++ src/core/gpio/f4/mod.rs
@@ -1,12 +1,33 @@
 pub mod f401;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum StmF4PinMode {
     InputMode(StmF4InputMode),
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum StmF4InputMode {
     Analog,
+
     Floating,
+
     PullUp,
+
     PullDown,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum StmF4OutputSpeed {
+    /// ~4 МГц. Для медленных сигналов.
+    Low,
+
+    /// ~25 МГц. Для общего применения.
+    Medium,
+
+    /// ~50 МГц. Для быстрого SPI, SDIO.
+    High,
+
+    /// ~100 МГц. Для LTDC, FMC, USB HS.
+    /// На Black Pill используйте с осторожностью - плата не рассчитана на такие частоты.
+    VeryHigh,
 }
