20457f95ebc7a514863538462ea3f6790e3d8015
aragami3070 <smirnov17612@gmail.com>
2026-07-01 02:23:51 +0400

    feat(core-config): add spi config

--- src/core/config.rs
+++ src/core/config.rs
@@ -1,7 +1,25 @@
-use crate::core::gpio::{ChosenPinWithMode};
+use crate::core::gpio::{ChosenPin, ChosenPinWithMode};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PinConfig {
     pub pin: ChosenPinWithMode,
-    pub label: Option<String>
+    pub label: Option<String>,
+}
+
+// Периферия
+pub struct SpiConfig {
+    pub enabled: bool,
+    pub frequency_mhz: u32,
+    pub mode: SpiMode,
+    pub sck: ChosenPin, // просто идентификатор пина
+    pub miso: Option<ChosenPin>,
+    pub mosi: Option<ChosenPin>,
+    pub nss: Option<ChosenPin>,
+}
+
+pub enum SpiMode {
+    Mode0, // CPOL=0, CPHA=0
+    Mode1, // CPOL=0, CPHA=1
+    Mode2, // CPOL=1, CPHA=0
+    Mode3, // CPOL=1, CPHA=1
 }
