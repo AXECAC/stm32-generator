de3b84f54b118644caa05bb6e3c68c621f686739
aragami3070 <smirnov17612@gmail.com>
2026-07-02 19:17:01 +0400

    feat(ethernet-w5500): add SocketMode

--- src/core/peripherals/ethernet/w5500.rs
+++ src/core/peripherals/ethernet/w5500.rs
@@ -9,3 +9,14 @@ pub struct NetworkConfig {
     pub subnet: Ipv4Addr,
     pub gateway: Ipv4Addr,
 }
+
+/// Режим работы сокета W5500.
+///
+/// Пока поддерживается только TCP-сервер.
+pub enum SocketMode {
+    /// Слушаем входящие TCP-подключения на указанном порту.
+    TcpServer {
+        port: u16,
+        socket_num: u8, // 0..=7, W5500 поддерживает 8 независимых сокетов
+    },
+}
