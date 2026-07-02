55326ad7f59a6d6d9bf2c1dce44a57223995ca1c
aragami3070 <smirnov17612@gmail.com>
2026-07-02 16:25:09 +0400

    feat(core-config): add more comments

--- src/core/config.rs
+++ src/core/config.rs
@@ -1,12 +1,14 @@
 use crate::core::gpio::{ChosenPin, ChosenPinWithMode};
 
+/// Конфигурация для одного пина из gpio платы микроконтроллера
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PinConfig {
     pub pin: ChosenPinWithMode,
     pub label: Option<String>,
 }
 
-// Периферия
+/// Конфигурация периферии
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct SpiConfig {
     pub enabled: bool,
     pub frequency_mhz: u32,
@@ -17,9 +19,19 @@ pub struct SpiConfig {
     pub nss: Option<ChosenPin>,
 }
 
+/// Конфигурация шины SPI
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum SpiMode {
-    Mode0, // CPOL=0, CPHA=0
-    Mode1, // CPOL=0, CPHA=1
-    Mode2, // CPOL=1, CPHA=0
-    Mode3, // CPOL=1, CPHA=1
+    /// Polarity: IdleLow (CPOL=0)
+    /// Phase CaptureOnFirstTransition (CPHA=0)
+    Mode0,
+    /// Polarity: IdleLow (CPOL=0)
+    /// Phase: CaptureOnSecondTransition (CPHA=1)
+    Mode1,
+    /// Polarity: IdleHigh (CPOL=1)
+    /// Phase: CaptureOnFirstTransition (CPHA=0)
+    Mode2,
+    /// Polarity: IdleHigh (CPOL=1)
+    /// Phase: CaptureOnSecondTransition (CPHA=1)
+    Mode3,
 }
