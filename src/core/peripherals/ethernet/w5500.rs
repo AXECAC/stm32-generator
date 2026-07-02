2dce173c388abb9792f6b2520aea9b4cb2da9518
aragami3070 <smirnov17612@gmail.com>
2026-07-02 19:17:38 +0400

    feat(ethernet-w5500): add W5500Config

--- src/core/peripherals/ethernet/w5500.rs
+++ src/core/peripherals/ethernet/w5500.rs
@@ -1,6 +1,6 @@
 use std::net::Ipv4Addr;
 
-use crate::core::peripherals::ethernet::MacAddr;
+use crate::core::{gpio::ChosenPin, peripherals::ethernet::MacAddr};
 
 /// Сетевые параметры
 pub struct NetworkConfig {
@@ -19,4 +19,24 @@ pub enum SocketMode {
         port: u16,
         socket_num: u8, // 0..=7, W5500 поддерживает 8 независимых сокетов
     },
+}
+
+/// Конфигурация модуля W5500.
+pub struct W5500Config {
+    // SPI пины
+    pub sck: ChosenPin,
+    pub miso: ChosenPin,
+    pub mosi: ChosenPin,
+
+    // Управляющие пины
+    pub cs: ChosenPin,  // Chip Select
+    pub rst: ChosenPin, // Hardware Reset
+
+    /// Настройки шины
+    pub spi_frequency_mhz: u32,
+
+    pub network: NetworkConfig,
+
+    /// Режим работы сокета
+    pub socket_mode: SocketMode,
 }
