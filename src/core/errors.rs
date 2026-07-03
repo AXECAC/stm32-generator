128a670668b9b8b3467c7231b9f459ce02bef81b
aragami3070 <smirnov17612@gmail.com>
2026-07-04 01:09:32 +0400

    feat(core-errors): add SpiBusNotFound variant in ConfigError

--- src/core/errors.rs
+++ src/core/errors.rs
@@ -8,6 +8,9 @@ pub enum ConfigError {
     #[error("Дублирующийся SPI: {0:?}")]
     DuplicateSpiBus(ChosenSpiBus),
 
+    #[error("SPI шина использованная в переферии не найдена: {0:?}")]
+    SpiBusNotFound(ChosenSpiBus),
+
     #[error("Конфликт в использовании уже занятого пина: {0:?}")]
     PinConflict(ChosenPin),
 }
