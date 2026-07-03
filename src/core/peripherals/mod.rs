b78338b15c3ae5008ff330d3e3d2cc8e11d8b8d2
aragami3070 <smirnov17612@gmail.com>
2026-07-04 00:26:08 +0400

    feat(core-peripherals): impl UsesPins trait for Peripheral

--- src/core/peripherals/mod.rs
+++ src/core/peripherals/mod.rs
@@ -1,8 +1,16 @@
-use crate::core::peripherals::ethernet::w5500::W5500Config;
+use crate::core::{UsesPins, peripherals::ethernet::w5500::W5500Config};
 
 pub mod ethernet;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Peripheral {
     W5500(W5500Config),
+}
+
+impl UsesPins for Peripheral {
+    fn uses_pins(&self) -> Vec<super::gpio::ChosenPin> {
+        match self {
+            Self::W5500(w5500) => w5500.uses_pins(),
+        }
+    }
 }
