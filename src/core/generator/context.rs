90e1d5a7e92c05de7d9d3d46e23865114b897a84
aragami3070 <smirnov17612@gmail.com>
2026-07-17 02:20:35 +0400

    feat(generator-context): add GpioPinCtx

--- src/core/generator/context.rs
+++ src/core/generator/context.rs
@@ -1,6 +1,16 @@
 use serde::Serialize;
 
 #[derive(Serialize)]
+pub struct GpioPinCtx {
+    pub label: String,
+    pub port: String,
+    pub pin_num: String,
+    pub method: String,
+    pub is_output: bool,
+    pub speed: Option<String>,
+}
+
+#[derive(Serialize)]
 pub struct PinCtx {
     pub port: String,
     pub pin_num: String,
