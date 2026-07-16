118b86281d45a70a064c9209f7a38740e1807d47
aragami3070 <smirnov17612@gmail.com>
2026-07-17 03:15:21 +0400

    feat(core-generator): add render jinja env in code fn

--- src/core/generator/mod.rs
+++ src/core/generator/mod.rs
@@ -1,11 +1,16 @@
 use crate::core::errors::GeneratorError;
+use crate::core::generator::context::TemplateContext;
 use minijinja::Environment;
 use std::collections::HashMap;
 
 pub mod context;
 pub mod templates;
 
-fn build_environment() -> Result<Environment<'static>, GeneratorError> {
+/// Создает [`minijinja::Environment`] и регистрирует в нём все статические шаблоны.
+///
+/// Имена шаблонов сохраняют суффикс `.j2`, так как они используются для связи
+/// модулей внутри самих шаблонов (через `{% include %}`).
+fn build_environment<'a>() -> Result<Environment<'a>, GeneratorError> {
     let mut env = Environment::new();
 
     // Точки входа
@@ -42,4 +47,39 @@ fn build_environment() -> Result<Environment<'static>, GeneratorError> {
     )?;
 
     Ok(env)
+}
+
+/// Финальный путь к файлу в проекте
+type ProjectPath = String;
+
+/// Готовый сгенерированный, с помощью [`minijinja`], исходный код
+type Code = String;
+
+/// Рендерит все файлы проекта, подставляя переданный `TemplateContext`.
+///
+/// # Errors
+/// Функция вернет ошибку [`GeneratorError::RenderError`], если рендер окажется
+/// неудачным (в .j2 файле не закрытая скобка, не правильный формат и тп)
+fn render_templates(
+    env: &Environment,
+    context: &TemplateContext,
+) -> Result<HashMap<ProjectPath, Code>, GeneratorError> {
+    let mut files = HashMap::new();
+
+    let cargo_toml = env.get_template("Cargo.toml.j2")?.render(context)?;
+    files.insert("Cargo.toml".to_string(), cargo_toml);
+
+    let cargo_config = env.get_template(".cargo/config.toml.j2")?.render(context)?;
+    files.insert(".cargo/config.toml".to_string(), cargo_config);
+
+    let memory_x = env.get_template("memory.x.j2")?.render(context)?;
+    files.insert("memory.x".to_string(), memory_x);
+
+    let justfile = env.get_template("justfile.j2")?.render(context)?;
+    files.insert("justfile".to_string(), justfile);
+
+    let main_rs = env.get_template("main.rs.j2")?.render(context)?;
+    files.insert("src/main.rs".to_string(), main_rs);
+
+    Ok(files)
 }
