cd03969c2093ed58a7b6da813f621adeb6f2a846
aragami3070 <smirnov17612@gmail.com>
2026-07-17 02:21:39 +0400

    feat(generator-context): add SpiCtx

--- src/core/generator/context.rs
+++ src/core/generator/context.rs
@@ -16,6 +16,18 @@ pub struct PinCtx {
     pub pin_num: String,
 }
 
+#[derive(Serialize)]
+pub struct SpiCtx {
+    pub bus_name: String,
+    pub pac_bus: String,
+    pub sck: PinCtx,
+    pub miso: Option<PinCtx>,
+    pub mosi: Option<PinCtx>,
+    pub polarity: String,
+    pub phase: String,
+    pub frequency_mhz: u32,
+    pub pins_tuple: String,
+}
 #[derive(Serialize, Default)]
 pub struct SocketModeCtx {
     pub tcp_server: Option<TcpServerCtx>,
