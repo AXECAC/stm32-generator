8222c7def7dab5e780a40e82b2787e2bff88a2c4
aragami3070 <smirnov17612@gmail.com>
2026-07-16 22:54:26 +0400

    feat(generator-templates): add entry point consts

---
+++ src/core/generator/templates.rs
@@ -0,0 +1,6 @@
+// Точки входа
+pub(crate) const CARGO_TOML: &str = include_str!("../../../assets/templates/Cargo.toml.j2");
+pub(crate) const CARGO_CONFIG_TOML: &str = include_str!("../../../assets/templates/.cargo/config.toml.j2");
+pub(crate) const MEMORY_X: &str = include_str!("../../../assets/templates/memory.x.j2");
+pub(crate) const JUSTFILE: &str = include_str!("../../../assets/templates/justfile.j2");
+pub(crate) const MAIN_RS: &str = include_str!("../../../assets/templates/main.rs.j2");
