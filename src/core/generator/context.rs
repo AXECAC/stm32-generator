e1fab038bd904835524894d2315bb4d9cdc68223
aragami3070 <smirnov17612@gmail.com>
2026-07-17 02:18:02 +0400

    feat(generator-context): add SocketModeCtx

--- src/core/generator/context.rs
+++ src/core/generator/context.rs
@@ -1,3 +1,10 @@
+use serde::Serialize;
+
+#[derive(Serialize, Default)]
+pub struct SocketModeCtx {
+    pub tcp_server: Option<TcpServerCtx>,
+}
+
 #[derive(Serialize)]
 pub struct TcpServerCtx {
     pub port: u16,
