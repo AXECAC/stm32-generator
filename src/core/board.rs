7e8afae944e0b54d4eee11a2e464699abfe36874
aragami3070 <smirnov17612@gmail.com>
2026-07-22 14:33:34 +0400

    feat(core-board): add Pin and PinType

---
+++ src/core/board.rs
@@ -0,0 +1,15 @@
+use crate::core::gpio::{ChosenPin, TargetMcu};
+use serde::Serialize;
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize)]
+pub enum PinType {
+    Gpio(ChosenPin),
+    Power,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize)]
+pub struct Pin {
+    pub pin_type: PinType,
+    pub label: String,
+    pub key: String,
+}
