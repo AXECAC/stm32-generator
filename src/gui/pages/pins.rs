efd4c5b94e4047d94ea7ec8262667b0791d8f358
aragami3070 <smirnov17612@gmail.com>
2026-07-23 18:56:44 +0400

    feat(pages-pins): init PinsPageModel

--- src/gui/pages/pins.rs
+++ src/gui/pages/pins.rs
@@ -1,12 +1,32 @@
 use crate::core::board::{Pin, PinType, TargetBoard};
 use crate::core::config::{Config, PinConfig};
-use crate::core::gpio::f4::{StmF4InputMode, StmF4OutputMode, StmF4OutputSpeed, StmF4PinMode};
-use crate::core::gpio::{ChosenPin, ChosenPinWithMode};
+use crate::core::gpio::{ChosenPinWithMode, PinModeUiInfo};
 use crate::gui::components::chip_canvas::{ChipCanvasInput, ChipCanvasModel, ChipCanvasOutput};
+use crate::gui::components::property_row::{PropertyRowModel, PropertyRowOutput};
 use adw::prelude::*;
+use relm4::factory::FactoryVecDeque;
 use relm4::{
     Component, ComponentController, ComponentParts, ComponentSender, Controller, RelmWidgetExt,
     SimpleComponent, adw, gtk,
 };
-use strum::VariantNames;
 
+/// Модель страницы настройки пинов (GPIO).
+///
+/// Отвечает за хранение локального состояния конфигуратора:
+/// текущего выбранного на холсте пина, его базового режима работы, текстового алиаса,
+/// а также управляет динамически формируемым списком дополнительных свойств.
+pub struct PinsPageModel {
+    pub config: Config,
+    pub selected_pin: Option<Pin>,
+    pub current_alias: String,
+
+    pub current_mode: Option<ChosenPinWithMode>,
+
+    pub pin_type_model: gtk::StringList,
+
+    pub alias_buffer: gtk::EntryBuffer,
+
+    pub dynamic_properties: FactoryVecDeque<PropertyRowModel>,
+
+    pub chip_canvas: Controller<ChipCanvasModel>,
+}
