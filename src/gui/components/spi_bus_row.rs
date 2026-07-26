d7e69628dc060eb9184fc4cb78e5f7a042509e9b
aragami3070 <smirnov17612@gmail.com>
2026-07-26 16:49:43 +0400

    feat(spi-bus-row): add SpiBusRowModel struct

---
+++ src/gui/components/spi_bus_row.rs
@@ -0,0 +1,19 @@
+use crate::core::config::SpiConfig;
+use crate::core::gpio::ChosenSpiBus;
+use adw::prelude::*;
+use relm4::factory::FactoryComponent;
+use relm4::{adw, gtk};
+
+/// Factory-модель строки сконфигурированной SPI-шины.
+///
+/// Используется для компактного отображения уже добавленных SPI шин и отправки
+/// запроса на удаление выбранной шины.
+#[derive(Debug)]
+pub struct SpiBusRowModel {
+    /// Идентификатор SPI-шины, к которой относится строка.
+    bus: ChosenSpiBus,
+    /// Заголовок строки, отображаемый в UI.
+    title: String,
+    /// Краткое описание параметров SPI-шины.
+    subtitle: String,
+}
